e0d1e64c1e589e9f04ae16568a667ab0bb13205a
Kent Chan <kntchn@gmail.com>
2020-01-04 02:45:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  array.detect {|entry| (entry == cheese_types(0)) || (entry == cheese_types(1)) || (entry == cheese_types(2))}
+  array.detect {|entry| (entry == cheese_types[0]) || (entry == cheese_types[1]) || (entry == cheese_types[2])}
 
 end
